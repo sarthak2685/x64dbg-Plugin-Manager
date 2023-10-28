baf888a3aaf8143defda79ff75405b76a412a9c2
Sarthak Rai <106958899+sarthak2685@users.noreply.github.com>
2023-10-28 13:17:11 +0530

    Create linklist.cpp

---
+++ linklist.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        head = nullptr;
+    }
+
+    // Function to insert a new node at the end of the linked list
+    void insert(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+
+    // Function to display the linked list
+    void display() {
+        Node* current = head;
+        while (current != nullptr) {
+            std::cout << current->data << " -> ";
+            current = current->next;
+        }
+        std::cout << "nullptr" << std::endl;
+    }
+};
+
+int main() {
+    LinkedList linkedList;
+
+    linkedList.insert(1);
+    linkedList.insert(2);
+    linkedList.insert(3);
+
+    std::cout << "Linked List: ";
+    linkedList.display();
+
+    return 0;
+}
